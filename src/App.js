f6e79088c76b88bd05a43827bc77615fd2d01383
Chinu7077 <97742083+Chinu7077@users.noreply.github.com>
2022-09-13 15:33:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Main from "./Components/Main";
+import './Components/style.css';
+import  './App.css';
+
+
+
+function App() {
+  return (
+    
+    <>
+    <img
+          className="header__logo"
+          src="https://www.pngall.com/wp-content/uploads/4/Pokemon-Pokeball-PNG-Photo.png"
+        />
+                <img
+          className="home__image"
+          src="https://fontmeme.com/temporary/2e8e4b5b137f4bdbba12e931d85405f9.png"
+          alt=""
+        />
+
+    
+    <Main/>
+    </>
+  );
+}
+
+export default App;
